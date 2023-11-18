d3b477cbac0e8141da910b61ccb39d95c29719cf
Артем <artikus.sol@gmail.com>
2023-11-18 17:50:20 +0400

    upd(acl 1.2.0): исправление спрособов отключения функционала Woo

--- classes/class-woocommerce-disabled.php
+++ classes/class-woocommerce-disabled.php
@@ -6,10 +6,6 @@ class Woocommerce_Disabled {
 
 	public function init_hooks(): void {
 
-		// Отключение раздела Маркетинг
-		add_filter( 'woocommerce_marketing_menu_items', '__return_empty_array' );
-		add_filter( 'woocommerce_admin_features', [ $this, 'disable_features' ] );
-
 		// Отключение раздела ноисов
 		add_filter( 'woocommerce_helper_suppress_admin_notices', '__return_true' );
 
@@ -19,14 +15,14 @@ class Woocommerce_Disabled {
 		// Отключение стилй панели Вукомерса
 		add_action( 'admin_enqueue_scripts', [ $this, 'disable_wc_admin_app' ], 19 );
 
-		// Скрытие верхнего бара на страницах Вукомерса
-		add_action( 'admin_head', [ $this, 'hide_header' ] );
+		add_filter( 'woocommerce_admin_get_feature_config', [ $this, 'feature_config' ], PHP_INT_MAX, 1 );
 
 		// Отклбчение виджетов Вукомерса в Консоли
 		add_action( 'wp_dashboard_setup', [ $this, 'disable_woocommerce_status' ], PHP_INT_MAX );
 		add_action( 'admin_menu', [ $this, 'disable_woocommerce_status' ], PHP_INT_MAX );
 		add_action( 'wp_user_dashboard_setup', [ $this, 'disable_woocommerce_status' ], PHP_INT_MAX );
-		// Отклбчение раздела Дополнения
+
+		// Отключение раздела Дополнения
 		add_action( 'admin_menu', [ $this, 'remove_admin_addon_submenu' ], 999 );
 
 		// Отключение сбора статистики и отслеживания
@@ -34,6 +30,37 @@ class Woocommerce_Disabled {
 		add_filter( 'woocommerce_apply_user_tracking', '__return_false', 999 );
 		add_filter( 'woocommerce_apply_tracking', '__return_false', 999 );
 
+	}
+
+
+	/**
+	 * Отключение разделов Аналитика, Маркетинг и страницы Обзор
+	 *
+	 * @param $feature_config
+	 *
+	 * @return array
+	 */
+	public function feature_config( $feature_config ): array {
+
+		$feature_config['activity-panels']                   = false;
+		$feature_config['analytics']                         = false;
+		$feature_config['product-block-editor']              = false;
+		$feature_config['minified-js']                       = true;
+		$feature_config['navigation']                        = false;
+		$feature_config['homescreen']                        = false;
+		$feature_config['marketing']                         = false;
+		$feature_config['onboarding']                        = false;
+		$feature_config['onboarding-tasks']                  = false;
+		$feature_config['shipping-setting-tour']             = false;
+		$feature_config['shipping-smart-defaults']           = false;
+		$feature_config['subscriptions']                     = false;
+		$feature_config['store-alerts']                      = false;
+		$feature_config['transient-notices']                 = false;
+		$feature_config['customize-store']                   = false;
+		$feature_config['customer-effort-score-tracks']      = false;
+		$feature_config['new-product-management-experience'] = false;
+
+		return $feature_config;
 	}
 
 
@@ -50,23 +77,7 @@ class Woocommerce_Disabled {
 
 		wp_dequeue_style( 'wc-admin-app' );
 		wp_deregister_style( 'wc-admin-app' );
-		?>
-		<style>.woocommerce-layout__header {
-				display: none;
-			}</style>
-		<?php
-	}
 
-
-	public function hide_header(): void {
-
-		?>
-		<style>
-			.woocommerce-layout__header {
-				display: none;
-			}
-		</style>
-		<?php
 	}
 
 
@@ -83,7 +94,9 @@ class Woocommerce_Disabled {
 
 		remove_submenu_page( 'woocommerce', 'wc-admin' );
 		remove_submenu_page( 'woocommerce', 'wc-addons' );
+		remove_submenu_page( 'woocommerce', 'wc-reports' );
 		remove_submenu_page( 'woocommerce', 'wc-addons&section=helper' );
+		remove_submenu_page( 'woocommerce', 'wc-admin&path=/extensions' );
 	}
 
 }
