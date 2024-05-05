c0b5cd6704719427fedda31a0de976c4587da37c
Артем <artikus.sol@gmail.com>
2024-05-05 20:14:10 +0400

    refactor(acl 2.0.0): правки стилей и полей настроек

--- src/Admin/Options.php
+++ src/Admin/Options.php
@@ -185,7 +185,7 @@ class Options {
 		add_action( 'admin_init', [ $this, 'admin_init' ] );
 
 		// Menu.
-		add_action( 'admin_menu', [ $this, 'admin_menu' ] );
+		add_action( 'admin_menu', [ $this, 'admin_menu' ], - PHP_INT_MAX);
 	}
 
 
@@ -729,11 +729,16 @@ class Options {
 
 		$value = esc_attr( $this->get_option( $args['id'], $args['section'], $args['std'] ) );
 		$html  = '<fieldset>';
+
 		$html  .= sprintf( '<label for="wposa-%1$s[%2$s]">', $args['section'], $args['id'] );
 		$html  .= sprintf( '<input type="hidden" name="%1$s[%2$s]" value="off" />', $args['section'], $args['id'] );
 		$html  .= sprintf( '<input type="checkbox" class="wposa-field wposa-field--switch" id="wposa-%1$s[%2$s]" name="%1$s[%2$s]" value="on" %3$s />', $args['section'],
-			$args['id'], checked( $value, 'on', false ) );
-		$html  .= sprintf( '%1$s</label>', $args['desc'] );
+			$args['id'],
+			checked( $value, 'on', false )
+		);
+
+		$html  .= sprintf( '%1$s</label>', $this->get_field_description( $args  ));
+
 		$html  .= '</fieldset>';
 
 		echo wp_kses( $html, self::ALLOWED_HTML );
